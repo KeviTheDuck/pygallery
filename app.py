465f3a96e334404d04d18efb7b06a0b99d1a4318
Kinjal Kavish Boro <85004952+KeviTheDuck@users.noreply.github.com>
2022-06-24 19:34:37 +0530

    upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+
+app = Flask(__name__)
+
+css = True
+
+@app.route("/")
+def gallery():
+    image_names = os.listdir("static/pics")
+    return render_template("gallery.html",image_names=image_names,css=css)
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000,debug=True)
